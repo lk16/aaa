4031efc740fb2821be677f79e286204aa136e0a7
Luuk Verweij <luuk_verweij@msn.com>
2022-02-24 08:45:30 +0100

    refactor: inline literals in rewrite rules for aaa

--- lang/tokenizer/aaa.py
+++ lang/tokenizer/aaa.py
@@ -2,6 +2,7 @@ from enum import IntEnum, auto
 from typing import Dict, Optional
 
 from lang.tokenizer.generic import (
+    OrParser,
     Parser,
     ParseTree,
     RegexBasedParser,
@@ -22,23 +23,56 @@ class SymbolType(IntEnum):
     LITERAL = auto()
     LOOP = auto()
     BRANCH = auto()
-    IF = auto()
-    ELSE = auto()
-    END = auto()
-    WHILE = auto()
     STRING_LITERAL = auto()
     INTEGER_LITERAL = auto()
     BOOLEAN_LITERAL = auto()
-    DOUBLE_QUOTE = auto()
-    WHITESPACE_CHAR = auto()
-    NEWLINE = auto()
-    SPACE = auto()
+
+
+OPERATION_LITERALS = [
+    "-",
+    "!=",
+    ".",
+    "*",
+    "/",
+    "\\n",
+    "%",
+    "+",
+    "<",
+    "<=",
+    "=",
+    ">",
+    ">=",
+    "and",
+    "drop",
+    "dup",
+    "not",
+    "or",
+    "over",
+    "rot",
+    "strlen",
+    "substr",
+    "swap",
+]
 
 
 # Shorthand to keep REWRITE_RULES readable
 S = SymbolType
 
 REWRITE_RULES: Dict[IntEnum, Parser] = {
+    S.BOOLEAN_LITERAL: lit("true") | lit("false"),
+    S.INTEGER_LITERAL: RegexBasedParser("^[0-9]+"),
+    S.STRING_LITERAL: RegexBasedParser('^"([^\\\\]|\\\\("|n|\\\\))*"'),
+    S.IDENTIFIER: RegexBasedParser("^[a-z_]+"),
+    S.LITERAL: sym(S.BOOLEAN_LITERAL) | sym(S.INTEGER_LITERAL) | sym(S.STRING_LITERAL),
+    S.OPERATION: OrParser(*[lit(op) for op in OPERATION_LITERALS]),
+    S.WHITESPACE: RegexBasedParser("^[ \\n]+"),
+    S.BRANCH: cat(
+        lit("if"),
+        sym(S.FUNCTION_BODY),
+        opt(cat(lit("else"), sym(S.FUNCTION_BODY))),
+        lit("end"),
+    ),
+    S.LOOP: cat(lit("while"), sym(S.FUNCTION_BODY), lit("end")),
     S.FUNCTION_BODY: cat(
         sym(S.WHITESPACE),
         rep(
@@ -52,53 +86,6 @@ REWRITE_RULES: Dict[IntEnum, Parser] = {
             )
         ),
     ),
-    S.BRANCH: cat(
-        sym(S.IF),
-        sym(S.FUNCTION_BODY),
-        opt(cat(sym(S.ELSE), sym(S.FUNCTION_BODY))),
-        sym(S.END),
-    ),
-    S.LOOP: cat(sym(S.WHILE), sym(S.FUNCTION_BODY), sym(S.END)),
-    S.OPERATION: (  # TODO rewrite with RegexBasedParser
-        lit("-")
-        | lit("!=")
-        | lit(".")
-        | lit("*")
-        | lit("/")
-        | lit("\\n")
-        | lit("%")
-        | lit("+")
-        | lit("<")
-        | lit("<=")
-        | lit("=")
-        | lit(">")
-        | lit(">=")
-        | lit("and")
-        | lit("drop")
-        | lit("dup")
-        | lit("not")
-        | lit("or")
-        | lit("over")
-        | lit("rot")
-        | lit("strlen")
-        | lit("substr")
-        | lit("swap")
-    ),
-    S.LITERAL: sym(S.BOOLEAN_LITERAL) | sym(S.INTEGER_LITERAL) | sym(S.STRING_LITERAL),
-    S.STRING_LITERAL: RegexBasedParser('^"([^\\\\]|\\\\("|n|\\\\))*"'),
-    S.INTEGER_LITERAL: RegexBasedParser("^[0-9]+"),
-    S.WHITESPACE: rep(sym(S.WHITESPACE_CHAR)),
-    S.WHITESPACE_CHAR: sym(S.SPACE) | sym(S.NEWLINE),
-    S.IDENTIFIER: RegexBasedParser("^[a-z_]+"),
-    S.BOOLEAN_LITERAL: lit("true") | lit("false"),
-    # TODO consider getting rid of all these literal symbols and just inline them above
-    S.SPACE: lit(" "),
-    S.NEWLINE: lit("\n"),
-    S.IF: lit("if"),
-    S.END: lit("end"),
-    S.WHILE: lit("while"),
-    S.ELSE: lit("else"),
-    S.DOUBLE_QUOTE: lit('"'),
 }
 
 ROOT_SYMBOL = S.FUNCTION_BODY
