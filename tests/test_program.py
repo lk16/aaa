7382a1ef91e0981b6c563338d7059f3524dce38f
Luuk Verweij <luuk_verweij@msn.com>
2022-07-11 20:42:31 +0200

    refactor: merge test files

--- tests/test_program.py
+++ tests/test_program.py
@@ -1,10 +1,14 @@
+import inspect
+import sys
 from pathlib import Path
 from unittest.mock import patch
 
 import pytest
 from pytest import MonkeyPatch
 
+from lang.instructions.types import Instruction
 from lang.runtime.program import Program
+from lang.runtime.simulator import Simulator
 from lang.typing.exceptions import FileReadError, MissingEnvironmentVariable
 
 pytestmark = pytest.mark.no_builtins_cache
@@ -38,3 +42,17 @@ def test_program_load_builtins_file_not_found() -> None:
 def test_program_load_builtins_ok() -> None:
     program = Program.without_file("fn main { nop }")
     assert len(program.file_load_errors) == 0
+
+
+def test_program_implements_all_instructions() -> None:
+    instruction_types = {
+        obj
+        for _, obj in inspect.getmembers(sys.modules["lang.instructions.types"])
+        if inspect.isclass(obj)
+        and issubclass(obj, Instruction)
+        and obj is not Instruction
+    }
+
+    simulator = Simulator(Program.without_file("fn main { nop }"))
+    implemented_instructions = set(simulator.instruction_funcs.keys())
+    assert instruction_types == implemented_instructions
