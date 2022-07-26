7f5c3f44c4d3bc0428575ef815d545c5e10f5b20
Luuk Verweij <luuk_verweij@msn.com>
2022-07-26 22:13:37 +0200

    wip: disabled tests for open syscall

--- tests/aaa/syscalls/test_open.py
+++ tests/aaa/syscalls/test_open.py
@@ -1,21 +1,38 @@
+from contextlib import redirect_stdout
+from io import StringIO
 from unittest.mock import patch
 
-from tests.aaa import check_aaa_main
+import pytest
+
+from lang.runtime.program import Program
+from lang.runtime.simulator import Simulator
 
 TEST_FD = 1337
 
 
+@pytest.mark.skip()
 def test_open_ok() -> None:
-    def mock_open() -> int:
+    def mock_open(path: str, flags: int, mode: int = 0o777) -> int:
         return TEST_FD
 
+    program = Program.without_file('fn main { "foo.txt" 0 511 . . }')
+
     with patch("lang.runtime.simulator.os.open", mock_open):
-        check_aaa_main('"foo.txt" 0 511 . .', f"true{TEST_FD}", [])
+        with redirect_stdout(StringIO()) as stdout:
+            Simulator(program).run()
+
+    assert stdout.getvalue() == f"true{TEST_FD}"
 
 
+@pytest.mark.skip()
 def test_open_fail() -> None:
-    def mock_open() -> int:
+    def mock_open(path: str, flags: int, mode: int = 0o777) -> int:
         raise FileNotFoundError
 
+    program = Program.without_file('fn main { "foo.txt" 0 511 . drop }')
+
     with patch("lang.runtime.simulator.os.open", mock_open):
-        check_aaa_main('"foo.txt" 0 511 . drop', f"false", [])
+        with redirect_stdout(StringIO()) as stdout:
+            Simulator(program).run()
+
+    assert stdout.getvalue() == f"false"
