773618772bbe33cbdee08d7366fccdbf41c9c363
Luuk Verweij <luuk_verweij@msn.com>
2022-07-27 19:43:45 +0200

    fix: broken tests for open syscall

--- tests/aaa/syscalls/test_open.py
+++ tests/aaa/syscalls/test_open.py
@@ -2,37 +2,35 @@ from contextlib import redirect_stdout
 from io import StringIO
 from unittest.mock import patch
 
-import pytest
-
 from lang.runtime.program import Program
 from lang.runtime.simulator import Simulator
 
 TEST_FD = 1337
 
 
-@pytest.mark.skip()
 def test_open_ok() -> None:
     def mock_open(path: str, flags: int, mode: int = 0o777) -> int:
         return TEST_FD
 
-    program = Program.without_file('fn main { "foo.txt" 0 511 . . }')
+    program = Program.without_file('fn main { "foo.txt" 0 511 open . . }')
+    simulator = Simulator(program)
 
     with patch("lang.runtime.simulator.os.open", mock_open):
         with redirect_stdout(StringIO()) as stdout:
-            Simulator(program).run()
+            simulator.run()
 
     assert stdout.getvalue() == f"true{TEST_FD}"
 
 
-@pytest.mark.skip()
 def test_open_fail() -> None:
     def mock_open(path: str, flags: int, mode: int = 0o777) -> int:
         raise FileNotFoundError
 
-    program = Program.without_file('fn main { "foo.txt" 0 511 . drop }')
+    program = Program.without_file('fn main { "foo.txt" 0 511 open . drop }')
+    simulator = Simulator(program)
 
     with patch("lang.runtime.simulator.os.open", mock_open):
         with redirect_stdout(StringIO()) as stdout:
-            Simulator(program).run()
+            simulator.run()
 
-    assert stdout.getvalue() == f"false"
+    assert stdout.getvalue() == "false"
