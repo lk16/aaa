fa8027d50f1f8f84e6d7770a3e07e9274495b48d
Luuk Verweij <luuk_verweij@msn.com>
2022-10-08 12:53:07 +0200

    refactor: simplify grammar and use lalr instead of early, speeds up tests a lot

--- aaa/parser/__init__.py
+++ aaa/parser/__init__.py
@@ -5,8 +5,14 @@ from lark.lark import Lark
 AAA_GRAMMAR_PATH = Path(__file__).parent / "aaa.lark"
 
 aaa_builtins_parser = Lark(
-    open(AAA_GRAMMAR_PATH).read(), start="builtins_file_root", maybe_placeholders=True
+    open(AAA_GRAMMAR_PATH).read(),
+    start="builtins_file_root",
+    maybe_placeholders=True,
+    parser="lalr",
 )
 aaa_source_parser = Lark(
-    open(AAA_GRAMMAR_PATH).read(), start="regular_file_root", maybe_placeholders=True
+    open(AAA_GRAMMAR_PATH).read(),
+    start="regular_file_root",
+    maybe_placeholders=True,
+    parser="lalr",
 )
