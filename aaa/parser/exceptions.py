8d00010f5b9d40eda480bbdfa32b5e36546f56c8
Luuk Verweij <luuk_verweij@msn.com>
2022-10-09 12:03:40 +0200

    fix: remove keyword tests

--- aaa/parser/exceptions.py
+++ aaa/parser/exceptions.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 from lark.exceptions import UnexpectedInput
-from lark.lexer import Token
 
 from aaa import AaaException
 
@@ -22,18 +21,6 @@ class ParseException(ParserBaseException):
         context = self.parse_error.get_context(self.file.read_text())
 
         return f"{self.where()}: Could not parse file\n" + context
-
-
-class KeywordUsedAsIdentifier(ParserBaseException):
-    def __init__(self, *, token: Token, file: Path) -> None:
-        self.token = token
-        self.file = file
-
-    def where(self) -> str:
-        return f"{self.file}:{self.token.line}:{self.token.column}"
-
-    def __str__(self) -> str:
-        return f'{self.where()}: Can\'t use keyword "{self.token.value}" as identifier.'
 
 
 class FileReadError(ParserBaseException):
