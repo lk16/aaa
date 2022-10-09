9b522889b5f27d7febcd3c3928082e8e8334300b
Luuk Verweij <luuk_verweij@msn.com>
2022-10-09 15:01:23 +0200

    chore: remove dead code

--- aaa/simulator/debug.py
+++ aaa/simulator/debug.py
@@ -1,12 +1,5 @@
 from typing import Optional
 
-from aaa.simulator.variable import Variable
-
-
-def format_stack_item(var: Variable) -> str:  # pragma: nocover
-    # TODO consider using __repr__() methods
-    raise NotImplementedError
-
 
 def format_str(string: str, max_length: Optional[int] = None) -> str:  # pragma: nocover
     string = string.replace("\n", "\\n")
