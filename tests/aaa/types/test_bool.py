f2217e64aa1f90312b105e0a93f76bcbec6220c1
Luuk Verweij <luuk_verweij@msn.com>
2022-10-20 23:07:50 +0200

    wip: move most int and bool tests to testd_stdlib.aaa

--- tests/aaa/types/test_bool.py
+++ tests/aaa/types/test_bool.py
@@ -9,16 +9,6 @@ from tests.aaa import check_aaa_main
         pytest.param("true .", "true", id="print-true"),
         pytest.param("false .", "false", id="print-false"),
         pytest.param("bool .", "false", id="print-zero-value"),
-        pytest.param("false not .", "true", id="not-false"),
-        pytest.param("true not .", "false", id="not-true"),
-        pytest.param("true true and .", "true", id="true-and-true"),
-        pytest.param("true false and .", "false", id="true-and-false"),
-        pytest.param("false true and .", "false", id="false-and-true"),
-        pytest.param("false false and .", "false", id="false-and-false"),
-        pytest.param("true true or .", "true", id="true-or-true"),
-        pytest.param("true false or .", "true", id="true-or-false"),
-        pytest.param("false true or .", "true", id="false-or-true"),
-        pytest.param("false false or .", "false", id="false-or-false"),
     ],
 )
 def test_bool(code: str, expected_output: str) -> None:
