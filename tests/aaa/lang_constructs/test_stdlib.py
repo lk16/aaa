0455c7c283a18129f938c38e873f2a3b9a30d574
Luuk Verweij <luuk_verweij@msn.com>
2022-10-21 22:26:06 +0200

    fix: dont print progress on aaa tests for stdlib on success

--- tests/aaa/lang_constructs/test_stdlib.py
+++ tests/aaa/lang_constructs/test_stdlib.py
@@ -1,15 +1,11 @@
 import os
 from pathlib import Path
 
-from pytest import CaptureFixture
-
 from aaa.run_tests import TestRunner
 
 
-def test_stdlib(capsys: CaptureFixture[str]) -> None:
+def test_stdlib() -> None:
     stdlib_path = Path(os.environ["AAA_STDLIB_PATH"])
-    with capsys.disabled():
-        print()
-        exit_code = TestRunner(stdlib_path).run()
 
+    exit_code = TestRunner(stdlib_path).run()
     assert exit_code == 0
