790169c71c9da2253627a0980f7475d683671dd3
Luuk Verweij <luuk_verweij@msn.com>
2023-01-28 15:44:14 +0100

    feat: simplify parser exceptions

--- aaa/parser/exceptions.py
+++ aaa/parser/exceptions.py
@@ -20,11 +20,6 @@ class ParserException(ParserBaseException):
         self.expected_token_types = expected_token_types
         self.found_token_types = found_token_type
 
-    def context(self) -> str:  # pragma: nocover
-        code = self.position.file.read_text()
-        line = code.split("\n")[self.position.line - 1]
-        return line + "\n" + ((self.position.column - 1) * " ") + "^\n"
-
     def __str__(self) -> str:  # pragma: nocover
         expected = ", ".join(
             token_type.name for token_type in self.expected_token_types
@@ -36,7 +31,7 @@ class ParserException(ParserBaseException):
         if len(self.expected_token_types) > 1:
             message += "one of "
 
-        message += f"{expected}, but found {found}\n" + self.context() + "\n"
+        message += f"{expected}, but found {found}\n" + self.position.context() + "\n"
         return message
 
 
@@ -45,7 +40,6 @@ class EndOfFileException(ParserBaseException):
         self.file = file
 
     def __str__(self) -> str:
-        # TODO add context
         return f"{self.file} Parsing failed, unexpected end of file."
 
 
@@ -54,14 +48,9 @@ class NewParserUnhandledTopLevelToken(ParserBaseException):
         self.position = position
         self.token_type = token_type
 
-    def context(self) -> str:  # pragma: nocover
-        code = self.position.file.read_text()
-        line = code.split("\n")[self.position.line - 1]
-        return line + "\n" + ((self.position.column - 1) * " ") + "^\n"
-
     def __str__(self) -> str:  # pragma: nocover
         return (
             f"{self.position}: Parsing failed, unexpected top-level token {self.token_type.name}\n"
-            + self.context()
+            + self.position.context()
             + "\n"
         )
